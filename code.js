469ec7a0952330cc030232064b32bfe7110666bc
Jacob Paivarinta <jlpaivar@mtu.edu>
2018-08-08 16:07:11 -0400

    Added waiting for document to be ready with JQuery

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
+$(document).ready();
+
 if (true) {
     var lizardBase = [];
     lizardBase.push("http://api.ning.com/files/UZ57-5HN3sbQY6PHfzH2293AnrPFVjwk-MPCkFjmPmSYYFnt2yNBjJqAQ8bu68JRfaRMsQpTahsVdRn5uIEEqyQ**7*raaFP/GreenLizards.jpg");
