657aa8cf83405916fe2149205ab3296f84f6043a
Jacob Paivarinta <jlpaivar@mtu.edu>
2018-08-08 16:13:12 -0400

    Set the lizard image get thing to a function to be fired when doc is ready

--- code.js
+++ code.js
@@ -1,15 +1,22 @@
-$(document).ready();
 
-if (true) {
+function getLizard () {
     var lizardBase = [];
     lizardBase.push("http://api.ning.com/files/UZ57-5HN3sbQY6PHfzH2293AnrPFVjwk-MPCkFjmPmSYYFnt2yNBjJqAQ8bu68JRfaRMsQpTahsVdRn5uIEEqyQ**7*raaFP/GreenLizards.jpg");
     lizardBase.push("http://animals.sandiegozoo.org/sites/default/files/2016-11/animals_hero_lizards.jpg");
 	// lizardBase.push("");
-	console.log("lizardBase length :  Should be 2 : Is " + lizardBase.length);
+
+	//console.log("lizardBase length :  Should be 2 : Is " + lizardBase.length);
+
 	var randInt = parseInt(lizardBase.length * Math.random());
-	console.log("lizardBase fetch :  Fetching element " + randInt + "  :  Element retrieved?");
+
+	//console.log("lizardBase fetch :  Fetching element " + randInt + "  :  Element retrieved?");
+
 	var imageLocale = lizardBase[randInt];
-	console.log(imageLocale);
-	console.log(document.getElementById("lizhard").src);
+
+	//console.log(imageLocale);
+	//console.log(document.getElementById("lizhard").src);
+
 	document.getElementById("lizhard").src = imageLocale;
-    }
+}
+
+$(document).ready(getLizard);
