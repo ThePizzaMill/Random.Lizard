dbc3f3d0b1c55991a3ce67e19779563c9b4a469a
Jacob Paivarinta <jlpaivar@mtu.edu>
2018-08-08 14:21:41 -0400

    Minor Changes

--- code.js
+++ code.js
@@ -2,6 +2,7 @@ if (true) {
     var lizardBase = [];
     lizardBase.push("http://api.ning.com/files/UZ57-5HN3sbQY6PHfzH2293AnrPFVjwk-MPCkFjmPmSYYFnt2yNBjJqAQ8bu68JRfaRMsQpTahsVdRn5uIEEqyQ**7*raaFP/GreenLizards.jpg");
     lizardBase.push("http://animals.sandiegozoo.org/sites/default/files/2016-11/animals_hero_lizards.jpg");
+    // lizardBase.push("");
     
     var ranNumber = parseInt(lizardBase.length * Math.random());
     var image = document.getElementByID();
