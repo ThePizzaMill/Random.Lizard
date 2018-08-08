cdce372ae284434348be601e7509a344168ebb32
Jacob Paivarinta <jlpaivar@mtu.edu>
2018-08-08 11:12:36 -0400

    Set up RandomLizard.html and the JS for it

---
+++ code.js
@@ -0,0 +1,7 @@
+if (true) {
+    var lizardBase = [];
+    lizardBase.push("http://api.ning.com/files/UZ57-5HN3sbQY6PHfzH2293AnrPFVjwk-MPCkFjmPmSYYFnt2yNBjJqAQ8bu68JRfaRMsQpTahsVdRn5uIEEqyQ**7*raaFP/GreenLizards.jpg");
+    
+    ranNumber = parseInt(lizardBase.length * Math.random());
+    document.title = lizardBase[ranNumber];
+    }
