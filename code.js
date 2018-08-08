5e1ef6ea9d85d7586ac50df29c12d2bc1a31ece4
Jacob Paivarinta <jlpaivar@mtu.edu>
2018-08-08 15:39:05 -0400

    Debug statements added

--- code.js
+++ code.js
@@ -2,7 +2,12 @@ if (true) {
     var lizardBase = [];
     lizardBase.push("http://api.ning.com/files/UZ57-5HN3sbQY6PHfzH2293AnrPFVjwk-MPCkFjmPmSYYFnt2yNBjJqAQ8bu68JRfaRMsQpTahsVdRn5uIEEqyQ**7*raaFP/GreenLizards.jpg");
     lizardBase.push("http://animals.sandiegozoo.org/sites/default/files/2016-11/animals_hero_lizards.jpg");
-    // lizardBase.push("");
-    
-	document.getElementById("lizhard").src = lizardBase[parseInt(lizardBase.length * Math.random())];
+	// lizardBase.push("");
+	console.log("lizardBase length :  Should be 2 : Is " + lizardBase.length);
+	var randInt = parseInt(lizardBase.length * Math.random());
+	console.log("lizardBase fetch :  Fetching element " + randInt + "  :  Element retrieved?");
+	var imageLocale = lizardBase[randInt];
+	console.log(imageLocale);
+	console.log(document.getElementById("lizhard").getAttribute("src"));
+	document.getElementById("lizhard").getAttribute("src") = imageLocale;
     }
