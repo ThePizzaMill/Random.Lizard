d8a2b39ce7d99fdd53f2f02c0bed66998933bbe9
unknown <jlpaivar@mtu.edu>
2018-08-16 20:51:38 -0400

    Modified code to allow for different access types
    Signed-off-by: unknown <jlpaivar@mtu.edu>

--- code.js
+++ code.js
@@ -1,22 +1,29 @@
+/*************************
+ Authors: thepizzamill
+ *************************/
 
-function getLizard () {
-    var lizardBase = [];
-    lizardBase.push("http://api.ning.com/files/UZ57-5HN3sbQY6PHfzH2293AnrPFVjwk-MPCkFjmPmSYYFnt2yNBjJqAQ8bu68JRfaRMsQpTahsVdRn5uIEEqyQ**7*raaFP/GreenLizards.jpg");
-    lizardBase.push("http://animals.sandiegozoo.org/sites/default/files/2016-11/animals_hero_lizards.jpg");
-	// lizardBase.push("");
+var animalLocation = "lizards.json";
 
-	//console.log("lizardBase length :  Should be 2 : Is " + lizardBase.length);
+var xmlhttp = new XMLHttpRequest();
 
-	var randInt = parseInt(lizardBase.length * Math.random());
+xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+        var lizardBase = JSON.parse(this.responseText);
+        
+        //console.log("lizardBase length :  Should be 2 : Is " + lizardBase.length);
+        
+        var randInt = parseInt(lizardBase.length * Math.random());
+        
+        //console.log("lizardBase fetch :  Fetching element " + randInt + "  :  Element retrieved?");
+        
+        var imageLocale = lizardBase.urls[randInt];
+        
+        //console.log(imageLocale);
+        //console.log(document.getElementById("lizhard").src);
+        
+        document.getElementById("lizhard").src = imageLocale;
+    }
+};
 
-	//console.log("lizardBase fetch :  Fetching element " + randInt + "  :  Element retrieved?");
-
-	var imageLocale = lizardBase[randInt];
-
-	//console.log(imageLocale);
-	//console.log(document.getElementById("lizhard").src);
-
-	document.getElementById("lizhard").src = imageLocale;
-}
-
-$(document).ready(getLizard);
+xmlhttp.open("GET", "lizards.json", true);
+xmlhttp.send();
